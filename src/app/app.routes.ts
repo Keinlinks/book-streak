d3c57fc410ab80a0c624834fc33b60608602d5d9
KevinClift <kevin.clift.b@gmail.com>
2025-02-01 02:17:47 -0300

    se arregla bug del boton de aregar libro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,28 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: ()=>import('./pages/home/home.component').then(m=>m.HomeComponent),
+    loadComponent: () =>
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
   },
   {
     path: 'add-book/:id',
-    loadComponent: ()=>import('./pages/add-book-form/add-book-form.component').then(m=>m.AddBookFormComponent),
+    loadComponent: () =>
+      import('./pages/add-book-form/add-book-form.component').then(
+        (m) => m.AddBookFormComponent
+      ),
+  },
+  {
+    path: 'add-book',
+    loadComponent: () =>
+      import('./pages/add-book-form/add-book-form.component').then(
+        (m) => m.AddBookFormComponent
+      ),
   },
   {
     path: 'config',
-    loadComponent: ()=>import('./pages/setting/setting.component').then(m=>m.SettingComponent),
-  }
+    loadComponent: () =>
+      import('./pages/setting/setting.component').then(
+        (m) => m.SettingComponent
+      ),
+  },
 ];
